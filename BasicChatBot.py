da16e6caeeff5c9fa45f35c56f5aa32ba1989b29
Kainat-Ali-Codingal <kainatali.codingal@gmail.com>
2025-06-14 21:18:50 +0500

    Activity 5

--- BasicChatBot.py
+++ BasicChatBot.py
@@ -3,6 +3,7 @@
 print("Hello! I'm your friendly chatbot.")
 name = input("What's your name? ")
 print(f"\nNice to meet you, {name}!\n")
+
 feeling = input("How are you feeling today? ")
 if "good" in feeling.lower() or "great" in feeling.lower():
     print("\nI'm glad to hear that!\n")
